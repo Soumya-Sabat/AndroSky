0e415868d5c143dbb1281db6743f31bac1e7a3de
SOUMYA <sabatsoumya05@gmail.com>
2026-06-13 19:36:11 +0530

    navbar

--- src/component/landing/Navbar.jsx
+++ src/component/landing/Navbar.jsx
@@ -1,5 +1,7 @@
 "use client";
+import { useRouter } from "next/navigation";
 export default function Navbar() {
+  const router = useRouter()
   return (
     <header className="fixed top-0 w-full z-50 bg-[#131315]/10 backdrop-blur-md border-b border-white/10 shadow-[0_0_20px_rgba(190,198,224,0.05)]">
       <nav className="flex justify-between items-center px-6 md:px-8 py-4 max-w-7xl mx-auto">
@@ -17,8 +19,12 @@ export default function Navbar() {
           <a className="text-sm font-medium text-[#c6c6cd] hover:text-[#bec6e0] hover:underline transition-colors" href="/galaxymap">Galaxy Map</a>
         </div>
         <div className="flex items-center gap-4">
-          {/* <button className="hidden lg:block text-[#c6c6cd] hover:text-[#bec6e0] transition-colors text-sm font-medium">Login</button> */}
-          <button className="button-gradient px-5 py-2 rounded-full font-['JetBrains_Mono'] text-white text-xs uppercase tracking-wider neon-glow-cyan active:scale-95">
+          <button 
+          onClick={() => router.push("/login")}
+          className="hidden lg:block text-[#c6c6cd] hover:text-[#bec6e0] transition-colors text-sm font-medium">Login</button>
+          <button 
+          onClick={() => router.push("/register")}
+          className="button-gradient px-5 py-2 rounded-full font-['JetBrains_Mono'] text-white text-xs uppercase tracking-wider neon-glow-cyan active:scale-95">
             Launch Odyssey
           </button>
         </div>
